745805e84219baf1865574b2d2e6a0338c61924e
Jhonatan-Negri <jhonatannegri659@gamil.com>
2025-10-16 21:55:42 -0300

    Transição para node

---
+++ introducao_js/type-string.js
@@ -0,0 +1,14 @@
+const primeiroText = "Olá, ";
+const segundoText = "mundo!";
+
+console.log(primeiroText + segundoText); 
+//Saida: Olá, mundo!
+
+console.log(primeiroText + " " + segundoText);
+//Saida: Olá, mundo!
+
+console.log(`${primeiroText}${segundoText}`);
+//Saida: Olá, mundo!
+
+console.log(`${primeiroText} ${segundoText}`);
+//Saida: Olá, mundo!    
