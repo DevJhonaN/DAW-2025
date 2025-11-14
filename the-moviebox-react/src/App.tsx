6ee53d7b3e5266ffc8f5d6b99154fd420405a84e
DevJhonaN <jhonatannegri659@gmail.com>
2025-11-13 22:28:01 -0300

    aula13-11

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -2,12 +2,16 @@ import { MovieContent } from "./cases/movies/components/movie-content"
 import { Footer } from "./components/layout/footer"
 import { Header } from "./components/layout/header"
 import { Navbar } from "./components/layout/navbar"
+<<<<<<< HEAD
 import { Highlight } from "./components/layout/highlight"
 import { MovieContextProvider } from "./cases/movies/context/movie-context"
+=======
+>>>>>>> parent of 5a0251f (ATVB2-2)
 
 function App() {
   return (
     <>
+<<<<<<< HEAD
       <Header />
       <main>
         <MovieContextProvider>
@@ -17,6 +21,14 @@ function App() {
         </MovieContextProvider>
       </main>
       <Footer />
+=======
+     <Header />
+     <main>
+      <Navbar/>
+      <MovieContent />
+     </main>
+     <Footer />
+>>>>>>> parent of 5a0251f (ATVB2-2)
     </>
   )
 }
